2f0f51d744a629bc7691bec680d374f3972e3fb3
Mario Perić <pericmario96@gmail.com>
2019-10-24 22:39:23 +0200

    Initial

---
+++ main.py
@@ -0,0 +1,78 @@
+from speech import *
+import speech_recognition as sr
+import requests
+from getch import getch
+import os
+import sys
+
+module_path = os.sep + ".." + os.sep + ".." + os.sep
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + module_path)
+import wolk
+
+
+if __name__ == "__main__":
+    node_ip = input("Enter node IP:")
+    r = sr.Recognizer()
+    list_microphones()
+    device = wolk.Device(key="4upvzcqmjdoatxw9", password="0c19526c-467c-40ca-98c2-e1ada607d872")
+
+    wolk_device = wolk.WolkConnect(device, host="demo.wolkabout.com")
+
+    # Establish a connection to the WolkAbout IoT Platform
+    print("Connecting to WolkAbout IoT Platform")
+    try:
+        wolk_device.connect()
+    except RuntimeError as e:
+        print(str(e))
+        sys.exit(1)
+
+    wolk_device.add_sensor_reading("S1", " ")
+    wolk_device.add_sensor_reading("S2", " ")
+    wolk_device.add_sensor_reading("S3", " ")
+    wolk_device.add_sensor_reading("S4", " ")
+    wolk_device.add_sensor_reading("S5", " ")
+    wolk_device.publish()
+
+    for i in range(5):
+        while True:
+            print("PRESS ANYTHING TO RECORD")
+            getch()
+            print("RECORDING")
+            with sr.Microphone(device_index=5) as source:
+                audio = r.listen(source, phrase_time_limit = 10)
+            sentence = ""
+            try:
+                sentence = recognize_voice(r, audio)
+            except TimeoutError:
+                print("TIMEOUT ERROR")
+            except sr.UnknownValueError:
+                print("SPEECH ERROR")
+            #except:
+            #    print("UNKNOWN ERROR")
+            print("sentence: %s" % sentence)
+            result = classify_sentence(sentence)
+            if result == SENTANCE_SAD:
+                text = "SAD"
+            elif result == SENTANCE_JOYFUL:
+                text = "JOYFUL"
+            elif result == SENTANCE_FUNNY:
+                text = "FUNNY"
+            elif result == SENTANCE_SERIOUS:
+                text = "SERIOUS"
+            else:
+                print("unknown result, assuming SERIOUS")
+                text = "SERIOUS"
+                result = SENTANCE_SERIOUS
+            print("result: %s" % text)
+            print("<s> to send, <r> to repeat")
+            ch = getch()
+            if ch == 's' or ch == 'S':
+                print("SENDING DATA %d" % i)
+                print(requests.get("http://%s/%d" % (node_ip, result)))
+                wolk_device.add_sensor_reading("S%d" % (i + 1), " ")
+                wolk_device.publish()
+                break
+            else:
+                print("REPEAT")
+        
+    print(classify_sentence("TIMISOARA IS CAPITAL"))
---
+++ speech.py
@@ -0,0 +1,50 @@
+import speech_recognition as sr
+import random
+
+JSON_PATH = r"hardsoftapplication-52182c5dd661.json"
+
+SENTANCE_SAD = 0
+SENTANCE_JOYFUL = 1
+SENTANCE_FUNNY = 2
+SENTANCE_SERIOUS = 3
+
+def list_microphones():
+    for index, name in enumerate(sr.Microphone.list_microphone_names()):
+        print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+
+def recognize_voice(r, audio):
+    with open(JSON_PATH) as f:
+        credentials = f.read()
+        text = r.recognize_google_cloud(audio, credentials_json= credentials)
+        return text.upper()
+
+def classify_sentence(sentence):
+    if "NOVI SAD" in sentence or "NOVISAD" in sentence or "TIMISOARA" in sentence or "SKOPJE" in sentence or "COCHLEA" in sentence or "SCOPELY" in sentence:
+        return SENTANCE_SERIOUS
+    elif "SERBIA" in sentence or "SERVIA" in sentence or "MACEDONIA" in sentence or "ROMANIA" in sentence:
+        return SENTANCE_SAD
+    elif "JAVA" in sentence or "C PLUS PLUS" in sentence or "C SHARP" in sentence or "C-SHARP" in sentence:
+        return SENTANCE_FUNNY
+    elif "WORK ABOUT" in sentence or "MICRO ELECTRONIC" in sentence or "CONTINENTAL" in sentence or "MIKRO" in sentence or "ELEKTRONIKA" in sentence or "WOKE ABOUT":
+        return SENTANCE_JOYFUL
+    elif "PLUS PLUS" in sentence:
+        return SENTANCE_FUNNY
+    elif "NOVI" in sentence:
+        return SENTANCE_SERIOUS
+    elif "SHARP" in sentence:
+        return SENTANCE_FUNNY
+    elif "NORTH" in sentence or "DERBY" in sentence:
+        return SENTANCE_SAD
+    elif "MICRO" in sentence or "ELECTRONIC" in sentence:
+        return SENTANCE_JOYFUL
+    elif "SCOPE" in sentence or "STAPLES" in sentence or "SCRUPLES" in sentence or "SCOPIA" in sentence:
+        return SENTANCE_SERIOUS
+    elif "WALK" in sentence or "WORK" in sentence or "ABOUT" in sentence or "WOKE" in sentence:
+        return SENTANCE_JOYFUL
+    elif "ROKU" in sentence or "BOUTS" in sentence or "CABANA" in sentence or "WORLD" in sentence or "WELL" in sentence:
+        return SENTANCE_JOYFUL
+    elif "SAD" in sentence or "SOARA" in sentence or "SORA" in sentence:
+        return SENTANCE_SERIOUS
+    else:
+        return random.choice([SENTANCE_SAD, SENTANCE_JOYFUL, SENTANCE_FUNNY, SENTANCE_SERIOUS])
+    
